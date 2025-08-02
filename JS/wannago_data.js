3a73bddeda946950a7deb7f60877e5a3940612c7
OY <yutaro.onuki9816@gmail.com>
2025-08-02 11:45:41 +0900

    no message

--- JS/wannago_data.js
+++ JS/wannago_data.js
@@ -208,6 +208,13 @@ const wannagoSpots = [
         image: "../IMG/沖縄フィード/BETTERGIRL.png",
         tags: ["朝食", "国際通り周辺"],
         map: "https://www.google.com/maps/search/?api=1&query=BETTERGIRL"
+      },
+      {
+        id: "aienbakery",
+        title: "AIENBAKERY",
+        image: "../IMG/沖縄フィード/AIENBAKERY.png",
+        tags: ["朝食", "南部"],
+        map: "https://www.google.com/maps/search/?api=1&query=AIENBAKERY"
       }
   ];
   
